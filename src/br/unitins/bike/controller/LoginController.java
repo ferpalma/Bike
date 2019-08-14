1fd9f2dac292973f4f86b28d2bc002e6cb5b7748
Janio <janiojunior@gmail.com>
2019-08-14 10:15:07 -0300

    tela de login

---
+++ src/br/unitins/bike/controller/LoginController.java
@@ -0,0 +1,34 @@
+package br.unitins.bike.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+
+	private String login = "Joao";
+	private String senha = "123";
+
+	public void logar() {
+		System.out.println(login);
+		System.out.println(senha);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
