9c44c79f9084b2126c01e2b40a2c2fa75d0c5251
Janio <janiojunior@gmail.com>
2019-11-06 01:40:02 -0300

    Telas: venda, carrinho, hostorico e detalhes (xhtml)

---
+++ src/br/unitins/bike/controller/TesteController.java
@@ -0,0 +1,40 @@
+package br.unitins.bike.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class TesteController{
+	
+	private String nome = "";
+	
+	private List<String> lista = null;
+	
+	public void add() {
+		getLista().add(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<String> getLista() {
+		if (lista == null) {
+			lista = new ArrayList<String>();
+			lista.add("Fredson");
+			lista.add("Marco");
+			lista.add("Silvano");
+		}
+		return lista;
+	}
+	
+}
